0ba4eab745a02dc34e6769bcfa46af25c9f5e55c
pc <pc@email.com>
2024-11-27 17:48:56 +0900

    자바 시작.

---
+++ HelloWorld/src/com/yedam/Hello.java
@@ -0,0 +1,14 @@
+package com.yedam;
+
+//대소문자 구분함
+public class Hello {
+	//메인 메소드.
+	public static void main(String[] args) {
+		//콘솔에 Hello,World 출력
+		System.out.println("Hello, World");
+		
+		//Hello.java -> Hello.class(실행파일)
+		//컴파일: 실행파일로 만드는것 (저장하면 eclipse 컴파일)
+		//HelloWorld 프로젝 - HelloWorld repository (두개 연결 시키는거	
+ }
+}
